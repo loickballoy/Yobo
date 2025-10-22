12f6fc81417494b791a8f2202a3b05400aaac393
Loick Balloy <loick.balloy@epita.fr>
2025-10-22 18:50:55 +0200

    fix: Binding port to Render

--- Backend/main.py
+++ Backend/main.py
@@ -3,7 +3,7 @@ import uvicorn
 if __name__ == "__main__":
     uvicorn.run(
         app="app.main:app",
-        host="localhost",
-        port=8000,
+        host="0.0.0.0",
+        port=5000,
         reload=True
     )
